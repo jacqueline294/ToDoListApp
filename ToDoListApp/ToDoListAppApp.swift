b51260a1ecd32ee23b3ba628ef46ff64ca157d65
jacqueline294 <144172392+jacqueline294@users.noreply.github.com>
2024-11-14 18:24:55 +0100

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by jacqueline Ngigi on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
